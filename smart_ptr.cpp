58f164f31921249ac13255eee59c283015bcd1f6
npb <npb@gmail.com>
2020-01-08 16:16:27 +0800

    update

--- smart_ptr.cpp
+++ smart_ptr.cpp
@@ -1,29 +1,44 @@
 #include <iostream>
-
+#include <string.h>
 
 template <typename T>
- 
- class smart_ptr
- {
+class smart_ptr
+{
  private:
      T *ptr_;
 
  public:
      smart_ptr();
-     ~smart_ptr() { delete ptr_; }
-
+     ~smart_ptr() {
+         std::cout << "delete ptr_" << std::endl;
+         delete ptr_; }
+     void log() { std::cout << "name === " << std::endl; }
      explicit smart_ptr(T *ptr = nullptr):ptr_(ptr){}
-
      T *get() const { return ptr_; }
+
+     T &operator*() const { return *ptr_; }
+     T *operator->() const { return ptr_; }
+     operator bool() const { return ptr_; }
  };
  
+  class test
+  {
+  private:
+  public:
+      test(){}
+      ~test(){}
+  };
   
  
-
-
+  
 
 int main()
 {
     std::cout << "test" << std::endl;
+
+    smart_ptr<char> p(new char[128]);
+    strncpy(p.get(), "hello",10);
+    std::cout << p.get() << std::endl;
+ 
     return 0;
 }
