642311ba0e30090937878c4c71d83b739a74f919
nongpubin <1050416698@qq.com>
2020-01-10 19:33:00 +0800

    容器

--- container.cpp
+++ container.cpp
@@ -5,6 +5,12 @@
 #include <vector>
 #include <map>
 
+
+#include <algorithm>
+#include <list>
+#include <queue>
+#include <stack>
+
 /*
 容器的共同点：
 1、容器都有开始和结束点。
@@ -49,19 +55,57 @@ int main()
     cout << "What's your name? ";
     getline(cin, name);
     cout << "Nice to meet you, " << name << "!\n";*/
-
+#if 0
    vector<obj1> v1;
    v1.reserve(2);
    v1.emplace_back();
    v1.emplace_back();
    v1.emplace_back();
 
+  vector<obj1> v3;
+   v3.reserve(2);
+   v3.emplace_back();
+  // v3.push_back(v1);
+   v3.emplace_back();
+   v3.emplace_back();
+
+   vector<std::string> name;
+   name.push_back("name");
+   name.push_back("hi");
+
+   cout << name << endl;
+   /*
    vector<obj2> v2;
    v2.reserve(2);
    v2.emplace_back();
    v2.emplace_back();
-   v2.emplace_back();
+   v2.emplace_back();*/
+#endif
+    
+    list<int> lst{1, 7, 2, 8, 3};
+    vector<int> vec{4, 7, 2, 8, 3};
 
+    sort(vec.begin(), vec.end());     
+     //sort(lst.begin(), lst.end());  //  会出错
+    lst.sort();                        
+
+    cout << vec << endl;
+    cout << lst << endl;
+
+    /*std::queue<int> q;  
+    q.push(1);  
+    q.push(2); 
+    q.push(3);  
+    while (!q.empty()) {    
+        std::cout << q.front()  << std::endl;    q.pop();  }*/
+
+    std::stack<int> s; 
+     s.push(1);  
+     s.push(2);  
+     s.push(3); 
+        while (!s.empty()) 
+        {  std::cout << s.top()  << std::endl;    
+        s.pop();  }
    return 0;
 }
 
